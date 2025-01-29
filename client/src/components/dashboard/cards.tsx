ff953a7c1e4b7a8bab0e311a5be50e6501eaf1e8
Juan Mardikian <software@mardikian.es>
2025-01-28 21:03:57 -0800

    fix: Average Lending Pool Risk Score calc

--- client/src/components/dashboard/cards.tsx
+++ client/src/components/dashboard/cards.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
-import { Pools } from "./dashboard-data-access"
+import { Pools, Predictions } from "./dashboard-data-access"
 
-export default function Cards({ poolsData }: { poolsData: Pools }) {
+export default function Cards({ poolsData, predictionsData }: { poolsData: Pools, predictionsData: Predictions }) {
 
     const [ items, setItems ] = useState([
         {
@@ -22,39 +22,49 @@ export default function Cards({ poolsData }: { poolsData: Pools }) {
         {
             key: "average_lending_pool_risk_score", 
             label: "Average Lending Pool Risk Score",
-            value: "100"
+            value: "1.00"
         }
     ])
 
     
     useEffect(() => {
 
-        const totalDepositValue = poolsData?.pools.reduce((total, obligation) => {
-            const depositSum = obligation.depositValueUSD;
-            return total + depositSum;
-        }, 0);
+        if( poolsData && predictionsData ) {
+            const totalDepositValue = poolsData?.pools.reduce((total, obligation) => {
+                const depositSum = obligation.depositValueUSD;
+                return total + depositSum;
+            }, 0);
+    
+            const totalBorrowedValue = poolsData?.pools.reduce((total, obligation) => {
+                const borrowedSum = obligation.borrowValueUSD
+                return total + borrowedSum;
+            }, 0);
+    
+            const totalHF = poolsData?.pools.reduce((total, obligation) => {
+                const sum = obligation.healthFactor;
+                return total + sum;
+            }, 0);
+            
+            const predictions = { ...predictionsData?.predictions };
 
-        const totalBorrowedValue = poolsData?.pools.reduce((total, obligation) => {
-            const borrowedSum = obligation.borrowValueUSD
-            return total + borrowedSum;
-        }, 0);
+            const data = poolsData?.pools?.flatMap((pool: any) =>
+                pool.deposits.map((deposit: any) => predictions[deposit.mint] / deposit.pricePerTokenInUSD)
+            );
 
-        const totalHF = poolsData?.pools.reduce((total, obligation) => {
-            const sum = obligation.healthFactor;
-            return total + sum;
-        }, 0);
+            const average_LPRC = data.reduce((acc, val) => acc + val, 0) / data.length;
+    
+    
+            setItems((prevItems) => {
+                const updatedItems = [...prevItems];
+                updatedItems[0].value = `$${(totalDepositValue || 0).toFixed(2)} USD`;
+                updatedItems[1].value = `$${(totalBorrowedValue || 0).toFixed(2)} USD`;
+                updatedItems[2].value = (totalHF || 1).toFixed(2);
+                updatedItems[3].value = (average_LPRC || 1).toFixed(2);
+                return updatedItems;
+            });
+        }
 
-
-
-        setItems((prevItems) => {
-            const updatedItems = [...prevItems];
-            updatedItems[0].value = `$${(totalDepositValue || 0).toFixed(2)} USD`;
-            updatedItems[1].value = `$${(totalBorrowedValue || 0).toFixed(2)} USD`;
-            updatedItems[2].value = (totalHF || 1).toFixed(2);
-            return updatedItems;
-        });
-
-    }, [ poolsData ])
+    }, [ poolsData, predictionsData ])
 
     return (
         <div className="w-full flex gap-2 overflow-x-auto">
@@ -62,7 +72,7 @@ export default function Cards({ poolsData }: { poolsData: Pools }) {
                 items.map((item) => (
                     <div key={item.key} className="rounded-xl bg-[#12181F] h-[49px] px-5 flex items-center gap-1">
                         <div className="text-[#C2C2C2] text-sm font-normal text-nowrap">{item.label} : </div>
-                        <div className="text-white text-sm font-bold">{item.value}</div>
+                        <div className="text-white text-sm font-bold text-nowrap">{item.value}</div>
                     </div>
                 ))
             }
